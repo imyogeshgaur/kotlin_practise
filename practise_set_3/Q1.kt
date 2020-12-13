133c59d3f087ae7f4209ec1feda36eaf04c99df5
imyogeshgaur <yogeshgaur1995@gmail.com>
2020-12-13 18:43:51 +0530

    Solved Q1 of Set 3

---
+++ practise_set_3/Q1.kt
@@ -0,0 +1,10 @@
+import java.util.Scanner
+
+fun main(){
+   print("Enter The String : ")
+   val sc =  Scanner(System.`in`)
+   var originalString = sc.nextLine()
+   var newString = originalString.toLowerCase()
+   print("The New String is : "+newString)
+   
+}
