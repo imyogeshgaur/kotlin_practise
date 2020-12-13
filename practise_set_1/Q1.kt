90d4d28cc37ace8634ca9d407b3bb35faa41229f
imyogeshgaur <yogeshgaur1995@gmail.com>
2020-12-13 18:42:10 +0530

    Solved Q1 of Set 1

---
+++ practise_set_1/Q1.kt
@@ -0,0 +1,12 @@
+
+fun main(){
+    print("Enter the First Number : ")
+    var a:Int = Integer.valueOf(readLine())
+    print("Enter the Second Number : ")
+    var b:Int = Integer.valueOf(readLine())
+    print("Enter the Third Number : ")
+    var c:Int = Integer.valueOf(readLine())
+
+    var sum: Int = a+b+c;
+    print("The Sum of Numbers is : "+sum)
+}
