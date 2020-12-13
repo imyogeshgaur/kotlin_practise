bd6dbc3e25418cc7e4eb18f93ef5c4f6dfbbd8dc
imyogeshgaur <yogeshgaur1995@gmail.com>
2020-12-13 18:44:04 +0530

    Solved Q2 of Set 3

---
+++ practise_set_3/Q2.kt
@@ -0,0 +1,9 @@
+import java.util.Scanner
+
+fun main(){
+    val sc =  Scanner(System.`in`)
+    print("Enter the String : ")
+    var originalString = sc.nextLine()
+    var newString = originalString.replace(" ","_")
+    print("The new String is : "+newString)
+}
