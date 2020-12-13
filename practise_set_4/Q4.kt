989c944c25885b461508ff1925467a39a8fe0000
imyogeshgaur <yogeshgaur1995@gmail.com>
2020-12-13 18:45:22 +0530

    Solved Q4 of Set 4

---
+++ practise_set_4/Q4.kt
@@ -0,0 +1,13 @@
+fun main() {
+    print("Enter Your Choice !!!")
+    var ch: Int = Integer.valueOf(readLine())
+    when (ch) {
+        1 -> print("Today is Monday")
+        2 -> print("Today is Tuesday")
+        3 -> print("Today is Wednesday")
+        4 -> print("Today is Thrusday")
+        5 -> print("Today is Friday")
+        6 -> print("Today is Saturday")
+        7 -> print("Today is Sunday")
+    }
+}
