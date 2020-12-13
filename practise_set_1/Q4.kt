81ecd5059f16bb2f78b8196e815b625ddf840af1
imyogeshgaur <yogeshgaur1995@gmail.com>
2020-12-13 18:42:56 +0530

    Solved Q4 of Set 1

---
+++ practise_set_1/Q4.kt
@@ -0,0 +1,6 @@
+fun main(){
+    print("Enter the Diatance in Kilometers : ")
+    var kilo:Int = Integer.valueOf(readLine())
+    var miles:Float = kilo/1.609f
+    print("The Disatnce in Miles is : "+miles)
+}
