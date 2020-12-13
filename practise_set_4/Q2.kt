d3ac135bcd0909ab6d640ac9344d0fde57391003
imyogeshgaur <yogeshgaur1995@gmail.com>
2020-12-13 18:44:38 +0530

    Solved Q2 of Set 4

---
+++ practise_set_4/Q2.kt
@@ -0,0 +1,21 @@
+fun main(){
+   print("Enter the Marks in Subject 1 : ")
+   var sub1:Int = Integer.valueOf(readLine())
+   print("Enter the Marks in Subject 2 : ")
+   var sub2:Int = Integer.valueOf(readLine())
+   print("Enter the Marks in Subject 3 : ")
+   var sub3:Int = Integer.valueOf(readLine())
+
+   val total:Int = sub1+sub2+sub3
+   
+   if(total/3>40 && sub1 >33){
+       print("You Are Passed !!!")
+    }else if(total/3>40 && sub2 >33){
+        print("You Are Passed !!!")
+    }else if(total/3>40 && sub3 >33){
+       print("You Are Passed !!!")
+   }else{
+       print("You are Fail !!!")
+   }
+
+}
