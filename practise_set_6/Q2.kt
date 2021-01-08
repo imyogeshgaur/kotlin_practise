333594ecdc695ea3f53efe0b89bd9cf93c75a629
imyogeshgaur <yogeshgaur1995@gmail.com>
2021-01-08 10:42:29 +0530

    Solved Q2 of set 6

---
+++ practise_set_6/Q2.kt
@@ -0,0 +1,10 @@
+fun main(){
+    var num = listOf(1,2,3,4,5)
+    println("Enter the Number You Wanna Find : ")
+    var numToFind:Int = Integer.valueOf(readLine())
+    if(num.contains(numToFind)){
+        println("The number ${numToFind} is Present in Array !!!")
+    }else{
+        println("The number ${numToFind} is not Present in Array !!!") 
+    }
+}
