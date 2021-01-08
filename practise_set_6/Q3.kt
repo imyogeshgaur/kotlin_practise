202fcb0af64b64abf71189b827432e5f20055dc3
imyogeshgaur <yogeshgaur1995@gmail.com>
2021-01-08 10:42:35 +0530

    Solved Q3 of set 6

---
+++ practise_set_6/Q3.kt
@@ -0,0 +1,9 @@
+fun main(){
+    val marks = arrayOf(10,20,30,40,50,60,70,80,90,100)
+    var sum = 0
+    for(i in marks){
+      sum+=i;
+  }
+  val avg = sum/10;
+  println("The Average of the Numner is : "+avg)
+}
