200a93fd7a50eca970555bb6f2cbc9eb8ff7f685
imyogeshgaur <yogeshgaur1995@gmail.com>
2021-01-08 14:33:24 +0530

    Solved Q2 of set 8

---
+++ practise_set_8/Q2.kt
@@ -0,0 +1,14 @@
+class CellPhone(){
+    fun vibrate(){
+      print("Ringing.....!!! \n")
+    }
+    fun ring(){
+      print("Vibrating....!!!")
+    }
+}
+
+fun main(){
+    val cell = CellPhone()
+    cell.vibrate()
+    cell.ring()
+}
