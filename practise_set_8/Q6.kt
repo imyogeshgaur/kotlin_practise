8b989a6f813cfbb0ba6a840e5f29a0b8662b7fae
imyogeshgaur <yogeshgaur1995@gmail.com>
2021-01-08 14:34:17 +0530

    Solved Q6 of set 8

---
+++ practise_set_8/Q6.kt
@@ -0,0 +1,18 @@
+class Circle(radius:Int){
+    var radiusOfCircle = radius
+    fun findArea(){
+       var areaOfCircle = Math.PI*radiusOfCircle*radiusOfCircle
+       print("The Area of the Circle is : $areaOfCircle")
+    }
+    fun findPerimeter(){
+       var perimeterOfCircle = 2*radiusOfCircle*Math.PI
+       print("The Area of the Circle is : $perimeterOfCircle")
+    }
+
+}
+
+fun main(){
+    val sq = Circle(2)
+    sq.findPerimeter()
+    sq.findArea()
+}
