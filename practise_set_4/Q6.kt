9ebd3dc49dcea1a7dd2ef803cca33d64f4a05b83
imyogeshgaur <yogeshgaur1995@gmail.com>
2021-01-09 11:39:04 +0530

    Solved Q6 of set 4

--- practise_set_4/Q6.kt
+++ practise_set_4/Q6.kt
@@ -1,16 +1,16 @@
 import java.util.Scanner
 
-fun main(){
+fun main() {
     var sc = Scanner(System.`in`)
     print("Enter the Domain of Websire : ")
     var domainName = sc.nextLine()
-    if(domainName.contains(".com")){
-       print("It is a Comercial Website !!")
-    }else if(domainName.contains(".org")){
+    if (domainName.contains(".com")) {
+        print("It is a Comercial Website !!")
+    } else if (domainName.contains(".org")) {
         print("It is a Orgnisation Website !!")
-    }else if(domainName.contains(".in")){
+    } else if (domainName.contains(".in")) {
         print("It is a Indian Website !!")
-    }else{
-      print("Undetectable Domain Extension!!")
+    } else {
+        print("Undetectable Domain Extension!!")
     }
 }
