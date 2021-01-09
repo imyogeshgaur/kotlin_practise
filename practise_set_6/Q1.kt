6a4960dc8e779115d7f4434d87bac78dba1216b6
imyogeshgaur <yogeshgaur1995@gmail.com>
2021-01-09 11:39:29 +0530

    Solved Q1 of set 6

--- practise_set_6/Q1.kt
+++ practise_set_6/Q1.kt
@@ -1,8 +1,8 @@
-fun main(){
-   var nums  = listOf(1.2f,2.3f,3.4f,4.5f,5.6f)
-   var sum:Float = 0.0f
-   for(i in nums){
-       sum += i
-   }
-   println("The Sum is ${sum}")
+fun main() {
+    var nums = listOf(1.2f, 2.3f, 3.4f, 4.5f, 5.6f)
+    var sum: Float = 0.0f
+    for (i in nums) {
+        sum += i
+    }
+    println("The Sum is ${sum}")
 }
