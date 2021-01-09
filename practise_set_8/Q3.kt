389eadafe100871ead0d2072d6722c191add938d
imyogeshgaur <yogeshgaur1995@gmail.com>
2021-01-09 11:41:44 +0530

    Solved Q3 of set 8

--- practise_set_8/Q3.kt
+++ practise_set_8/Q3.kt
@@ -1,17 +1,16 @@
-class Square(side:Int){
+class Square(side: Int) {
     var sideOfSquare = side
-    fun findArea(){
-       var areaOfSquare = sideOfSquare*sideOfSquare
-       print("The Area of the Square is : $areaOfSquare")
+    fun findArea() {
+        var areaOfSquare = sideOfSquare * sideOfSquare
+        print("The Area of the Square is : $areaOfSquare")
     }
-    fun findPerimeter(){
-       var perimeterOfSquare = 4*sideOfSquare
-       print("The Area of the Square is : $perimeterOfSquare")
+    fun findPerimeter() {
+        var perimeterOfSquare = 4 * sideOfSquare
+        print("The Area of the Square is : $perimeterOfSquare")
     }
-
 }
 
-fun main(){
+fun main() {
     val sq = Square(5)
     sq.findPerimeter()
     sq.findArea()
