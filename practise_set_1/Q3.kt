282eef55c6bdb63f9b5ad54b6360b861bfda063a
imyogeshgaur <yogeshgaur1995@gmail.com>
2021-01-09 11:36:32 +0530

    Solved Q3 of set 1

--- practise_set_1/Q3.kt
+++ practise_set_1/Q3.kt
@@ -1,5 +1,5 @@
-fun main(){
+fun main() {
     print("Enter Your Name : ")
     val name = readLine()
-    print("Hello "+name+" have a good day !!!")
+    print("Hello " + name + " have a good day !!!")
 }
