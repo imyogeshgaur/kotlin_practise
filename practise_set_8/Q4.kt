3bf432bd26b9626a9551fea701f064feeee0b647
imyogeshgaur <yogeshgaur1995@gmail.com>
2021-01-09 11:42:03 +0530

    Solved Q4 of set 8

--- practise_set_8/Q4.kt
+++ practise_set_8/Q4.kt
@@ -1,19 +1,18 @@
-class Rectangle(length:Int,breadth:Int){
+class Rectangle(length: Int, breadth: Int) {
     var lengthOfRectangle = length
     var breadthOfRectangle = breadth
-    fun findArea(){
-       var areaOfRectangle = lengthOfRectangle*breadthOfRectangle
-       print("The Area of Rectangle is : $areaOfRectangle")
+    fun findArea() {
+        var areaOfRectangle = lengthOfRectangle * breadthOfRectangle
+        print("The Area of Rectangle is : $areaOfRectangle")
     }
-    fun findPerimeter(){
-       var perimeterOfRectangle = 2*(lengthOfRectangle+breadthOfRectangle)
-       print("The Area of Rectangle is : $perimeterOfRectangle")
+    fun findPerimeter() {
+        var perimeterOfRectangle = 2 * (lengthOfRectangle + breadthOfRectangle)
+        print("The Area of Rectangle is : $perimeterOfRectangle")
     }
-
 }
 
-fun main(){
-    val rect = Rectangle(4,5)
+fun main() {
+    val rect = Rectangle(4, 5)
     rect.findPerimeter()
     rect.findArea()
 }
