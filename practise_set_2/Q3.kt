06ae8d1d7d9f4edb200ecc32a64be92591d8460b
imyogeshgaur <yogeshgaur1995@gmail.com>
2021-01-09 11:37:14 +0530

    Solved Q3 of set 2

--- practise_set_2/Q3.kt
+++ practise_set_2/Q3.kt
@@ -1,13 +1,12 @@
-fun main(){
+fun main() {
     print("Enter the Comprison Number : ")
-    var comp:Int = Integer.valueOf(readLine())
+    var comp: Int = Integer.valueOf(readLine())
     print("Enter the Number : ")
-    var numb:Int = Integer.valueOf(readLine())
+    var numb: Int = Integer.valueOf(readLine())
 
-    if(comp>numb){
+    if (comp > numb) {
         print("The number is Samller !!!")
-    }
-    else{
+    } else {
         print("The Number is Greater !!!")
     }
 }
