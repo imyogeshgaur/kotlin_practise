dbb660f9585fcf2da66a54328f98772b439f1c16
imyogeshgaur <yogeshgaur1995@gmail.com>
2021-01-09 11:36:40 +0530

    Solved Q4 of set 1

--- practise_set_1/Q4.kt
+++ practise_set_1/Q4.kt
@@ -1,6 +1,6 @@
-fun main(){
+fun main() {
     print("Enter the Diatance in Kilometers : ")
-    var kilo:Int = Integer.valueOf(readLine())
-    var miles:Float = kilo/1.609f
-    print("The Disatnce in Miles is : "+miles)
+    var kilo: Int = Integer.valueOf(readLine())
+    var miles: Float = kilo / 1.609f
+    print("The Disatnce in Miles is : " + miles)
 }
