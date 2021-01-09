24dfa8f2400c65ddd50ebf3af88b9cdcb490c2e0
imyogeshgaur <yogeshgaur1995@gmail.com>
2021-01-09 11:41:24 +0530

    Solved Q1 of set 8

--- practise_set_8/Q1.kt
+++ practise_set_8/Q1.kt
@@ -1,16 +1,16 @@
-class Employee(){
-    fun setSalary(sal:Int){
-       val salaryOfEmployee = sal
-       println("The Salary of the Employee is : "+salaryOfEmployee)
-    }
-    fun setname(name:String){
-      val nameOfEmployee = name
-      println("The Name of the Employee is :"+nameOfEmployee)
-    }
+class Employee() {
+  fun setSalary(sal: Int) {
+    val salaryOfEmployee = sal
+    println("The Salary of the Employee is : " + salaryOfEmployee)
+  }
+  fun setname(name: String) {
+    val nameOfEmployee = name
+    println("The Name of the Employee is :" + nameOfEmployee)
+  }
 }
 
-fun main(){
-    val emp = Employee()
-    emp.setname("Yogesh")
-    emp.setSalary(56)
+fun main() {
+  val emp = Employee()
+  emp.setname("Yogesh")
+  emp.setSalary(56)
 }
