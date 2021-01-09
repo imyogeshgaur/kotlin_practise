cfcb6cb3956726ea1c027b77816150fa3ccb7990
imyogeshgaur <yogeshgaur1995@gmail.com>
2021-01-09 11:38:45 +0530

    Solved Q3 of set 4

--- practise_set_4/Q3.kt
+++ practise_set_4/Q3.kt
@@ -2,16 +2,16 @@ fun main() {
     print("Enter the Salary : ")
     val salary: Int = Integer.valueOf(readLine())
 
-    if (salary <250000) {
+    if (salary < 250000) {
         print("You are exempted from Tax !!!")
-    } else if (salary >= 250000 && salary <500000) {
-        val taxPayed:Int = salary / 20
+    } else if (salary >= 250000 && salary < 500000) {
+        val taxPayed: Int = salary / 20
         print("The Tax paid by You is : " + taxPayed)
-    } else if (salary >= 500000 && salary <1000000) {
-        val taxPayed:Int  = salary / 5
+    } else if (salary >= 500000 && salary < 1000000) {
+        val taxPayed: Int = salary / 5
         print("The Tax paid by You is : " + taxPayed)
     } else {
-        val taxPayed:Double  = salary * 0.3
+        val taxPayed: Double = salary * 0.3
         print("The Tax paid by You is : " + taxPayed)
     }
 }
