3da8f97e8f4cebc4a803a1c5a78af03d8f47975b
imyogeshgaur <yogeshgaur1995@gmail.com>
2021-01-09 11:38:08 +0530

    Solved Q2 of set 3

--- practise_set_3/Q2.kt
+++ practise_set_3/Q2.kt
@@ -1,9 +1,9 @@
 import java.util.Scanner
 
-fun main(){
-    val sc =  Scanner(System.`in`)
+fun main() {
+    val sc = Scanner(System.`in`)
     print("Enter the String : ")
     var originalString = sc.nextLine()
-    var newString = originalString.replace(" ","_")
-    print("The new String is : "+newString)
+    var newString = originalString.replace(" ", "_")
+    print("The new String is : " + newString)
 }
