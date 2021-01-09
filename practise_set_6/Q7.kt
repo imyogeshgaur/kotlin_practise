c1b53457516274e84071d20192055936b172f808
imyogeshgaur <yogeshgaur1995@gmail.com>
2021-01-09 11:40:24 +0530

    Solved Q7 of set 6

--- practise_set_6/Q7.kt
+++ practise_set_6/Q7.kt
@@ -1,5 +1,5 @@
-fun main(){
-    val myarray:Array<Int> = arrayOf(1,2,3,4,5,6,7,8,9,10)
+fun main() {
+    val myarray: Array<Int> = arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
     val min = myarray.min()
     print("The minimum element is : $min ")
 }
