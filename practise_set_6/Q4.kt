565e72d8b6e41ddfd74c83d7d8e635be2c5c1527
imyogeshgaur <yogeshgaur1995@gmail.com>
2021-01-09 11:39:59 +0530

    Solved Q4 of set 6

--- practise_set_6/Q4.kt
+++ practise_set_6/Q4.kt
@@ -1,21 +1,20 @@
-fun main(){
+fun main() {
     val row = 2
-    val column=3
-    val mat1 = arrayOf(intArrayOf(2,3,4),intArrayOf(5,2,3))
-    val mat2 = arrayOf(intArrayOf(1,7,9),intArrayOf(6,2,0))
+    val column = 3
+    val mat1 = arrayOf(intArrayOf(2, 3, 4), intArrayOf(5, 2, 3))
+    val mat2 = arrayOf(intArrayOf(1, 7, 9), intArrayOf(6, 2, 0))
 
-    val res = Array(row) {IntArray(column)}
-    for(i in 0..row-1){
-       for(j in 0..column-1){
-           res [i][j] = mat1[i][j]+mat2[i][j];
-       }
-    } 
+    val res = Array(row) { IntArray(column) }
+    for (i in 0..row - 1) {
+        for (j in 0..column - 1) {
+            res[i][j] = mat1[i][j] + mat2[i][j]
+        }
+    }
     println("The Resultant of the Matrix is : ")
-    for(i in res){
-        for(j in i){
+    for (i in res) {
+        for (j in i) {
             print("$j   ")
         }
         println()
     }
-
 }
