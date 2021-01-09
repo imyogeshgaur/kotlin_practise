d862b91d8f23e45fb44143ecdf863f55d5684358
imyogeshgaur <yogeshgaur1995@gmail.com>
2021-01-09 11:38:54 +0530

    Solved Q5 of set 4

--- practise_set_4/Q5.kt
+++ practise_set_4/Q5.kt
@@ -3,7 +3,7 @@ fun main() {
     val year: Int = Integer.valueOf(readLine())
     if (year % 4 == 0) {
         print("The Year is a Leap Year !!!")
-    }else{
+    } else {
         print("The Year is not Leap Year !!!")
     }
 }
